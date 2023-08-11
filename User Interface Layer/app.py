ed653c1e604713c9e181c053594c2921e7c031db
TatendaKim <tatendakbeni@gmail.com>
2023-08-11 17:33:25 +0200

    Remittance_Calulator.html file now has code, still working on validating it

--- User Interface Layer/app.py
+++ User Interface Layer/app.py
@@ -2,26 +2,26 @@ from flask import Flask, render_template, request, redirect, url_for, session
 import hashlib
 
 app = Flask(__name__)
-app.secret_key = '1234abcd'  
+app.secret_key = '1234abcd'
 
 # Simulating user data storage (Replace with a proper database)
 users = {
-    'user1': {
+    'tatendakbeni@gmail.com': {
         'password': hashlib.sha256('password123'.encode()).hexdigest()
     }
 }
 
 # Login route
-@app.route('/http://127.0.0.1:5000')
+@app.route('/')
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        username = request.form['username']
+        email = request.form['email']
         password = request.form['password']
         hashed_password = hashlib.sha256(password.encode()).hexdigest()
 
-        if username in users and users[username]['password'] == hashed_password:
-            session['username'] = username
+        if email in users and users[email]['password'] == hashed_password:
+            session['email'] = email
             return redirect(url_for('dashboard'))
 
     return render_template('login.html')
@@ -29,14 +29,14 @@ def login():
 # Dashboard route
 @app.route('/dashboard')
 def dashboard():
-    if 'username' in session:
-        return f'Welcome, {session["username"]}! This is your dashboard.'
-    return redirect(url_for('login'))
+    if 'email' in session:
+        return f'Welcome, {session["email"]}! This is your dashboard.'
+    return render_template('remittance_calculator.html')
 
 # Logout route
 @app.route('/logout')
 def logout():
-    session.pop('username', None)
+    session.pop('email', None)
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
